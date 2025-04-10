30844b363ea969dbdcc34c860287a97dc3ebaf3c
tamsilkhan <tamsilk.khan@gmail.com>
2025-04-10 11:47:25 -0400

    Updated password with process env variables

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,11 +1,13 @@
 // emailConfig.js
+require('dotenv').config();
+
 module.exports = {
   host: "smtp.zoho.com", // Zoho SMTP server
   port: 465, // Use 465 for SSL or 587 for TLS
   secure: true, // true for 465, false for other ports
   auth: {
-    user: "noreply@briskmfg.com", // Your Zoho email address
-    pass: "1FAed0sa24JE", // Your Zoho email password
+    user: process.env.EMAIL_USER, // Your Zoho email address
+    pass: process.env.EMAIL_PASS, // Your Zoho email password
   },
   from: "noreply@briskmfg.com", // Sender email address
   to: [
